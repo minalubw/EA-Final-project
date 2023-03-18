ec1640ce496b2f3686d0c45ec9c6fe6c1a4adf17
minalubw <minalubiresaw@gmail.com>
2023-03-18 16:39:40 -0500

    Update MemberShip.java

--- src/main/java/edu/miu/badge/domains/MemberShip.java
+++ src/main/java/edu/miu/badge/domains/MemberShip.java
@@ -1,31 +1,14 @@
 package edu.miu.badge.domains;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class MemberShip {
+public class Membership {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="[membership_id]")
     private int id;
-    @Column(name="[start_date]", nullable = false)
-    private LocalDate startDate;
-    @Column(name="[end_date]", nullable = false)
-    private LocalDate endDate;
-    @ManyToOne
-    @JoinColumn(name="[plan_id]", nullable = false)
-    private Plan plan;
-    @ManyToOne
-    @JoinColumn(name="[plan_type_id]", nullable = false)
-    private PlanType planType;
-    @Column(name="[number_of_allowance]")
-    private int numberOfAllowance;
+
 }
