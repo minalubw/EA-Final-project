75c83387fc9c092c6c20aec9b8b12cb98e652442
manesakae <manesakae@gmail.com>
2023-03-18 15:20:13 -0500

    remove files

--- src/main/java/edu/miu/badge/repositories/MembershipRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.miu.badge.repositories;
-
-import edu.miu.badge.domains.Membership1;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MembershipRepository extends JpaRepository<Membership1, Integer> {
-}
