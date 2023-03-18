584d4a12b9ffbea896002fc51cc81bd6d9f94b0d
name <121846883+A1azar1@users.noreply.github.com>
2023-03-18 11:31:06 -0500

    init commit

---
+++ src/main/java/edu/miu/badge/repositories/MemberRepository.java
@@ -0,0 +1,10 @@
+package edu.miu.badge.repositories;
+
+
+import edu.miu.badge.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Integer> {
+}
