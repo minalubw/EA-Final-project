f28799974f6116d7c271211d0f090b8bd2c87ea7
Daniel T. Meresie <dani.2tse@gmail.com>
2023-03-18 17:13:33 -0500

    made some changes

--- src/main/java/edu/miu/badge/controllers/TimeSlotController.java
+++ src/main/java/edu/miu/badge/controllers/TimeSlotController.java
@@ -1,13 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package edu.miu.badge.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author Daniel Tsegay Meresie
  */
+@RestController
+@RequestMapping("/timeslot")
 public class TimeSlotController {
     
 }
--- src/main/java/edu/miu/badge/dto/TimeSlotDTO.java
+++ src/main/java/edu/miu/badge/dto/TimeSlotDTO.java
@@ -1,9 +1,19 @@
 package edu.miu.badge.dto;
 
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 /**
  *
  * @author Daniel Tsegay Meresie
  */
+@Setter @Getter @AllArgsConstructor
+@NoArgsConstructor @ToString
 public class TimeSlotDTO {
-    
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
 }
