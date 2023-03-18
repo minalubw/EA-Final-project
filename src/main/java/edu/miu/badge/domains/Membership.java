558ec81f1f6b4742ab3fce77a4d14a42fe78dff7
Daniel T. Meresie <dani.2tse@gmail.com>
2023-03-18 17:23:14 -0500

    changes added

--- src/main/java/edu/miu/badge/domains/Membership.java
+++ src/main/java/edu/miu/badge/domains/Membership.java
@@ -11,7 +11,7 @@ import java.time.LocalDate;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class MemberShip {
+public class Membership {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="[membership_id]")
