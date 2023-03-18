937eaa8fca69f16a4d59350fe29a9a2339300e9a
name <121846883+A1azar1@users.noreply.github.com>
2023-03-18 11:28:30 -0500

    deleted

--- src/main/java/edu/miu/badge/BadgeMembershipSystemApplication.java
+++ src/main/java/edu/miu/badge/BadgeMembershipSystemApplication.java
@@ -1,14 +1,14 @@
 package edu.miu.badge;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BadgeMembershipSystemApplication implements CommandLineRunner {
-@Autowired
-private PersonService personService;
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(BadgeMembershipSystemApplication.class, args);
 	}
@@ -16,8 +16,6 @@ private PersonService personService;
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		Person person = new Person(2, "John", "jhon@gmail.com");
-		personService.save(person);
-		System.out.println(personService.get(2));
+
 	}
 }
