28f68f3242c8b119a34b12008f3eb3ffa4314bba
Nirajan Karki <nirajanchelsea@gmail.com>
2023-03-17 21:09:49 -0500

    inital commit for ea project

---
+++ src/main/java/edu/miu/badge/BadgeMembershipSystemApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.badge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadgeMembershipSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadgeMembershipSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/miu/badge/BadgeMembershipSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.badge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadgeMembershipSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
