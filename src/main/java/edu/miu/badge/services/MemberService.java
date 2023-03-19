1f4d2e4ff2900ae0777a4170f696d2144fd07840
minalubw <minalubiresaw@gmail.com>
2023-03-19 15:43:35 -0500

    memeber update

--- src/main/java/edu/miu/badge/services/MemberService.java
+++ src/main/java/edu/miu/badge/services/MemberService.java
@@ -2,9 +2,13 @@ package edu.miu.badge.services;
 
 import edu.miu.badge.dto.MemberDTO;
 
+import java.util.List;
+
 public interface MemberService {
-    public void insertNewMember(MemberDTO memberDTO);
+    public MemberDTO insertNewMember(MemberDTO memberDTO);
     public MemberDTO getMemberById(int id);
     public String deleteMemberById(int id);
+    List<MemberDTO> getAllMembers();
+    MemberDTO updateMember(int id, MemberDTO memberDTO);
 
 }
