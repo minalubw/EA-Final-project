71967290262306074d4d92396b8d7ad3a97570e8
Daniel T. Meresie <dani.2tse@gmail.com>
2023-03-18 21:42:29 -0500

    completed crud for timeslots

--- src/main/java/edu/miu/badge/controllers/TimeSlotController.java
+++ src/main/java/edu/miu/badge/controllers/TimeSlotController.java
@@ -1,11 +1,19 @@
 package edu.miu.badge.controllers;
 
 import edu.miu.badge.domains.TimeSlot;
+import edu.miu.badge.dto.TimeSlotDTO;
 import edu.miu.badge.repositories.TimeSlotRepository;
+import edu.miu.badge.services.TimeSlotService;
+import java.time.LocalDateTime;
 import java.util.List;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,18 +24,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/timeslot")
 public class TimeSlotController {
-    
     @Autowired
-    TimeSlotRepository timeSlotRepository;
+    private ModelMapper mm;
+    @Autowired
+    private TimeSlotService timeSlotService;
     
     @GetMapping("/all")
     public List<TimeSlot> getAllTimeslots(){
-        return timeSlotRepository.findAll();
+        return timeSlotService.getAllTimeSlots();
     }
     
     @PostMapping("/")
-    public TimeSlot add(TimeSlot ts){
-        return timeSlotRepository.save(ts);
+    public TimeSlot add(@RequestBody TimeSlotDTO tsDTO){
+        return timeSlotService.createTimeSlot(tsDTO);
     }
-    
+    @GetMapping("/test")// this is just for testing...
+    public String testt(){
+        timeSlotService.createTimeSlot(new TimeSlotDTO(LocalDateTime.now(), LocalDateTime.now().plusDays(10)));
+        return "time slot added for test";
+    }
+    @PutMapping("/{id}")
+    public TimeSlot update(@PathVariable Long id, @RequestBody TimeSlotDTO timeSlotDTO){
+        return timeSlotService.updateTimeSlot(id, timeSlotDTO);
+    }
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        timeSlotService.deleteTimeSlot(id);
+    }
 }
