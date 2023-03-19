b3fb2290d2a5047ae8f58a2876bdb873eac0cf97
name <121846883+A1azar1@users.noreply.github.com>
2023-03-19 13:15:57 -0500

    Member Tweaks

--- src/main/java/edu/miu/badge/controllers/MemberController.java
+++ src/main/java/edu/miu/badge/controllers/MemberController.java
@@ -2,20 +2,21 @@ package edu.miu.badge.controllers;
 
 import edu.miu.badge.domains.HttpResponse;
 import edu.miu.badge.domains.Member;
+import edu.miu.badge.dto.MemberDTO;
+import edu.miu.badge.exceptions.MemberNotFoundException;
 import edu.miu.badge.services.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(path = "/members", produces = "application/json")
 public class MemberController {
     @Autowired
     private MemberService memberService;
 
-    @PostMapping("/members")
+    @PostMapping
     public ResponseEntity<HttpResponse> createMember(@RequestBody Member member){
         memberService.insertNewMember(member);
         return responseResponseEntity(HttpStatus.OK, "New member added Successfully!");
@@ -24,5 +25,23 @@ public class MemberController {
     private ResponseEntity<HttpResponse> responseResponseEntity(HttpStatus httpStatus, String message){
         HttpResponse httpResponse = new HttpResponse(httpStatus.value(), httpStatus, httpStatus.getReasonPhrase().toUpperCase(),  message);
         return new ResponseEntity<>(httpResponse, httpStatus);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getMemberById(@PathVariable int id){
+        try {
+            return new ResponseEntity<MemberDTO>(memberService.getMemberById(id), HttpStatus.OK);
+        }catch (MemberNotFoundException e){
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteMemberById(@PathVariable int id){
+        try {
+            return new ResponseEntity<String>(memberService.deleteMemberById(id), HttpStatus.OK);
+        }catch (MemberNotFoundException e){
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
     }
 }
--- src/main/java/edu/miu/badge/services/impl/MemberServiceImpl.java
+++ src/main/java/edu/miu/badge/services/impl/MemberServiceImpl.java
@@ -1,18 +1,44 @@
 package edu.miu.badge.services.impl;
 
 import edu.miu.badge.domains.Member;
+import edu.miu.badge.dto.MemberDTO;
+import edu.miu.badge.exceptions.MemberNotFoundException;
 import edu.miu.badge.repositories.MemberRepository;
 import edu.miu.badge.services.MemberService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class MemberServiceImpl implements MemberService {
 
     @Autowired
     private MemberRepository memberRepository;
+    @Autowired
+    ModelMapper modelMapper;
     @Override
     public void insertNewMember(Member member) {
         memberRepository.save(member);
+    }
+
+    @Override
+    public MemberDTO getMemberById(int id) {
+        Optional<Member> member = memberRepository.findById(id);
+        if(member.isPresent()){
+            return modelMapper.map(member.get(), MemberDTO.class);
+        }
+        throw new MemberNotFoundException("Member with id " + id + " not found");
+    }
+
+    @Override
+    public String deleteMemberById(int id) {
+        Optional<Member> member = memberRepository.findById(id);
+        if(member.isPresent()){
+            memberRepository.delete(member.get());
+            return "Member with id " + id + " deleted successfully";
+        }
+        throw new MemberNotFoundException("Member with id " + id + " not found");
     }
 }
