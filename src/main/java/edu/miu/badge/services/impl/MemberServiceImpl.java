ce41dbc41a1c38e90f4c59b228ae11f35500583a
Nirajan Karki <nirajanchelsea@gmail.com>
2023-03-20 14:18:22 -0500

    sample change

--- src/main/java/edu/miu/badge/services/impl/MemberServiceImpl.java
+++ src/main/java/edu/miu/badge/services/impl/MemberServiceImpl.java
@@ -9,12 +9,14 @@ import edu.miu.badge.services.MemberService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
+@Transactional
 public class MemberServiceImpl implements MemberService {
 
     @Autowired
