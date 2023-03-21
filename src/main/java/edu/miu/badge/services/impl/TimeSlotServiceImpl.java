8caa03179bd6e04d3c8a79a109d87d57ce2ea990
Daniel T. Meresie <dani.2tse@gmail.com>
2023-03-21 12:46:15 -0500

    exceptions added

--- src/main/java/edu/miu/badge/services/impl/TimeSlotServiceImpl.java
+++ src/main/java/edu/miu/badge/services/impl/TimeSlotServiceImpl.java
@@ -2,9 +2,11 @@ package edu.miu.badge.services.impl;
 
 import edu.miu.badge.domains.TimeSlot;
 import edu.miu.badge.dto.TimeSlotDTO;
+import edu.miu.badge.exceptions.TimeSlotNotFoundException;
 import edu.miu.badge.repositories.TimeSlotRepository;
 import edu.miu.badge.services.TimeSlotService;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,12 +31,14 @@ public class TimeSlotServiceImpl implements TimeSlotService{
     }
 
     @Override
-    public TimeSlot getTimeSlotById(Long id) {
-        return timeSlotRepository.findById(id).get();
+    public TimeSlot getTimeSlotById(Long id) throws TimeSlotNotFoundException{
+        return timeSlotRepository.findById(id).orElseThrow(
+                () -> new TimeSlotNotFoundException("TimeSlot with the ID " + id + " is not found")
+        );
     }
 
     @Override
-    public TimeSlot updateTimeSlot(Long id, TimeSlotDTO timeSlotDTO) {
+    public TimeSlot updateTimeSlot(Long id, TimeSlotDTO timeSlotDTO) throws TimeSlotNotFoundException{
         TimeSlot old = getTimeSlotById(id);
         old.setEndTime(timeSlotDTO.getEndTime());
         old.setStartTime(timeSlotDTO.getStartTime());
@@ -43,8 +47,10 @@ public class TimeSlotServiceImpl implements TimeSlotService{
     }
 
     @Override
-    public void deleteTimeSlot(Long id) {
+    public String deleteTimeSlot(Long id) throws TimeSlotNotFoundException{
         timeSlotRepository.delete(getTimeSlotById(id));
+        System.out.println("object deleted");
+        return "TimeSlot with the ID : " + id + " has been removed";
     }
 
     @Override
