912025bace4411ba64a7eb7e14265b086e583a8b
name <121846883+A1azar1@users.noreply.github.com>
2023-03-21 21:56:35 -0500

    small changes

--- src/main/java/edu/miu/badge/controllers/TransactionController.java
+++ src/main/java/edu/miu/badge/controllers/TransactionController.java
@@ -3,6 +3,7 @@ package edu.miu.badge.controllers;
 import edu.miu.badge.dto.RequestTransactionDTO;
 import edu.miu.badge.dto.ResponseTransactionDTO;
 import edu.miu.badge.exceptions.ResourceNotFoundException;
+import edu.miu.badge.exceptions.TransactionNotFoundException;
 import edu.miu.badge.services.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,13 +18,22 @@ public class TransactionController {
     TransactionService transactionService;
 
     @PostMapping("/transactions")
-    public ResponseEntity<?> createTransaction(@RequestBody RequestTransactionDTO requestTransactionDTO) throws Exception {
-        return new ResponseEntity<>(transactionService.createTransaction(requestTransactionDTO), HttpStatus.OK);
+    public ResponseEntity<?> createTransaction(@RequestBody RequestTransactionDTO requestTransactionDTO)  {
+        try{
+            return new ResponseEntity<>(transactionService.createTransaction(requestTransactionDTO), HttpStatus.OK);
+        }catch (TransactionNotFoundException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+
     }
     @GetMapping("/transactions/{id}")
     public ResponseEntity<?> getTransaction(@PathVariable int id) throws TransactionNotFoundException {
-        ResponseTransactionDTO transaction = transactionService.getTransaction(id);
-        return new ResponseEntity<>(transaction, HttpStatus.OK);
+        try{
+            ResponseTransactionDTO transaction = transactionService.getTransaction(id);
+            return new ResponseEntity<>(transaction, HttpStatus.OK);
+        }catch (TransactionNotFoundException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping("/transactions")
