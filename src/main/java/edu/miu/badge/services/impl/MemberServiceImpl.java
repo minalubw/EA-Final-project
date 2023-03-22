97b21e38ac7b2b220c6469bba1c5db2ae3e37e37
manesakae <manesakae@gmail.com>
2023-03-22 13:41:55 -0500

    remove print out

--- src/main/java/edu/miu/badge/services/impl/MemberServiceImpl.java
+++ src/main/java/edu/miu/badge/services/impl/MemberServiceImpl.java
@@ -65,7 +65,6 @@ public class MemberServiceImpl implements MemberService {
     @Override
     public ResponseMemberDTO getMemberById(int id) throws ResourceNotFoundException{
         Optional<Member> member = memberRepository.findById(id);
-        System.out.println("======"+member.get().getEmail());
         if(member.isPresent()){
             return modelMapper.map(member.get(), ResponseMemberDTO.class);
         }
